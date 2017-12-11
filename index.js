bb50e9bf21a2de34ae6c988f3a08ec79b63f4a2a
Sunny Kotwal <sunnykotwal@yahoo.com>
2017-12-11 17:25:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(holiday, name) {
   return (`Happy ${holiday}, ${name}!`)
 }
 
-function holidayCountdown(days, holiday) {
-  return (`It's ${days} days until ${holiday}!`
+function holidayCountdown(number, holiday) {
+  return (`It's ${number} days until ${holiday}!`
 }
